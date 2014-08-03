950fbaf08e96db9bc3d8d5a18be62058d7370771
Kunal Bhalla <kunalb@fb.com>
2014-08-03 03:46:27 -0700

    Add a quick way to run tests

---
+++ hyphenate.d
@@ -0,0 +1,14 @@
+/// Return suffix hyphenations for a given word.
+
+import std.stdio; 
+
+string[] rules = ["et-ic", "al-is-tic", "s-tic", "p-tic", "-lyt-ic", "ot-ic", "an-tic", "h-nic"];
+
+string[] hyphenate(string word) {
+  return ["test"];
+}
+
+unittest {
+  writeln("Hello, World");
+  assert(true);
+}
